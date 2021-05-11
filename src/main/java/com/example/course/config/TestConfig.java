2543a4e82fb6a71ebd9bcc1667f0828826980f70
higoramorim <1810042@aluno.univesp.br>
2021-05-10 21:38:08 -0300

    implementando entidade produto

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
+import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.CategoryRepository;
 import com.example.course.repositories.OrderRepository;
+import com.example.course.repositories.ProductRepository;
 import com.example.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,7 +41,15 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+	    Product p1 = new Product(null, "The lord of rings", "Lorem ipsum", 90.5, "");
+	    Product p2 = new Product(null, "smart tv", "Lorem ipsum", 890.5, "");
+	    Product p3 = new Product(null, "mac book pro", "Lorem ipsum", 8490.5, "");
+	    Product p4 = new Product(null, "IPhone XX", "Lorem ipsum", 3490.5, "");
+	    Product p5 = new Product(null, "Rails for Dummies", "Lorem ipsum", 190.5, "");
+		
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
